17b49da06e1c872a57aeb1700fa35e1a6afc48ec
soyorinn <3352794205@qq.com>
2026-07-30 20:03:44 +0800

    第五章：函数完成

---
+++ python_ai_base/chapter5/FirstFunction.py
@@ -0,0 +1,12 @@
+"""
+函数初体验
+ - 参数如果不需要,可以省略
+ - 返回值如果不需要,可以省略
+ - 函数必须先定义后使用
+"""
+def notice():
+    print("one")
+    print("two")
+    print("three")
+
+notice()
---
+++ python_ai_base/chapter5/FunctionWithParameter.py
@@ -0,0 +1,9 @@
+"""
+带参数的函数
+"""
+def add(x,y):
+    result = x + y
+    print(f"{x} + {y} = {result}")
+
+add(1,3)
+add(9,9)
